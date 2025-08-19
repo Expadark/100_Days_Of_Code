69bf0805df981ac3e3b83e1dac986631c5e31f5a
Expadark <119635690+Expadark@users.noreply.github.com>
2025-08-19 16:36:11 -0500

    Add files via upload

---
+++ Cypher.py
@@ -0,0 +1,61 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    def encrypt(original_text, shift1):
+        new_text = ""
+        for letter in original_text:
+            if letter in alphabet:
+                shifted_text = alphabet.index(letter) + shift1
+                if shifted_text >= 26:
+                    shifted_text -= 26
+                new_text += alphabet[shifted_text]
+            else:
+                new_text += letter
+        print(new_text)
+
+
+    def decrypt(encrypted, shift1):
+        decrypted_text = ""
+        for letter in encrypted:
+            if letter in alphabet:
+                shifted_text = alphabet.index(letter) - shift1
+                if shifted_text < 0:
+                    shifted_text += 26
+                decrypted_text += alphabet[shifted_text]
+            else:
+                decrypted_text += letter
+        print(decrypted_text)
+
+
+    if direction == "encode":
+        encrypt(text, shift)
+    elif direction == "decode":
+        decrypt(text, shift)
+
+    rs = input("Do you want to go again? Type 'yes' or no': ").lower()
+    if rs == "no":
+        should_continue = False
